4fa9fbe175457ecaf3fd5f6db139ee73cccb0c00
Iana Baraksina <baraksina.ya@gmail.com>
2025-06-20 19:48:31 +0200

    cleanup

--- task2/solution.py
+++ task2/solution.py
@@ -13,7 +13,6 @@ def parse_page(url):
     link = soup.find('a', string='Следующая страница')
     if link:
         next_url = link.attrs.get('href')
-
     result = {}
     for group in soup.select("#mw-pages .mw-category-group"):
         letter = group.find("h3").text.strip()
@@ -26,18 +25,15 @@ def parse_page(url):
 def get_animals():
     total = {}
     url = START_URL
-
     while url:
         next_url, result = parse_page(url)
         for letter, count in result.items():
             total[letter] = total.get(letter, 0) + count
         url = next_url
-
     return total
 
 
 stats = get_animals()
-
 with open("beasts.csv", "w", encoding="UTF-8") as file_out:
     for k, v in stats.items():
         file_out.write(f"{k},{v}\n")
